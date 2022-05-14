130a9cbe06f675177ebc594088838b7cddefaf1a
Tyler-Culp <tculp@ucsd.edu>
2022-05-13 18:18:51 -0700

    Update Account.java
    Created account constructor and getter/setter methods for balance

--- Account.java
+++ Account.java
@@ -1,8 +1,29 @@
 public class Account{
-  private int balance;
+  private double balance;
   private String accountName;
   
-  public createAccount(String name, int initialBalance){
-    
+  public Account(String name, double initialBalance){
+    this.accountName = name;
+    this.balance = initialBalance;
+  }
+
+  public double getBalance(){
+    return this.balance;
+  }
+
+  public void deposit(double depositAmount){
+    this.balance += depositAmount;
+  }
+
+  public double withdraw(double withdrawAmount){
+    if (this.balance >= withdrawAmount){
+      this.balance -= withdrawAmount;
+      return withdrawAmount;
+    }
+
+    else{
+      System.out.println("Not enough money in account!");
+      return 0;
+    }
   }
 }
